c7a3883684d504c3417acb3a09bc54413c30c826
aarya-saraf <aaryasaraf0412@gmail.com>
2026-04-21 23:50:58 +0530

    fix api key for streamlit deployment

--- llm_helper.py
+++ llm_helper.py
@@ -1,13 +1,9 @@
 from langchain_groq import ChatGroq
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 llm = ChatGroq(
-    groq_api_key=os.getenv("GROQ_API_KEY"),
+    groq_api_key=st.secrets["GROQ_API_KEY"],
     model_name="llama-3.3-70b-versatile"
-
 )
 
 if __name__ == "__main__":
